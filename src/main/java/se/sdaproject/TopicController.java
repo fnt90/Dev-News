af6e24797035d48160e439a57495495645ccd3c2
Fiona <thompsonfiona@outlook.com>
2021-04-09 20:05:16 +0200

    Add Delete mapping and Get article from topic

--- src/main/java/se/sdaproject/TopicController.java
+++ src/main/java/se/sdaproject/TopicController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class TopicController {
 
@@ -46,10 +48,26 @@ public class TopicController {
     //@DeleteMapping("/topics/{id}")
 
     //Delete association of a topic for one article
-    //@DeleteMapping("/articles/{articleId}/topics/{topicId}")
+    @DeleteMapping("/articles/{articleId}/topics/{topicId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteAssociation(@PathVariable Long topicId, @PathVariable Long articleId) {
+        Article article = articleRepository.findById(articleId).orElseThrow(ResourceNotFoundException::new);
+        Topic topic = topicRepository.findById(topicId).orElseThrow(ResourceNotFoundException::new);
+        if (topic.getArticles().contains(article)) {
+            topic.getArticles().remove(article);
+            topicRepository.save(topic);
+        } else {
+            throw new ResourceNotFoundException();
+        }
+    }
 
     //List all articles associated with one topic
-    //@GetMapping("/topics/{topicId}/articles")
+    @GetMapping("/topics/{topicId}/articles")
+    public ResponseEntity<List<Article>> listArticlesInTopic(@PathVariable Long topicId) {
+        Topic topic = topicRepository.findById(topicId).orElseThrow(ResourceNotFoundException::new);
+        List<Article> articles = topic.getArticles();
+        return ResponseEntity.ok(articles);
+    }
 
     /*GET	/topics
     * GET	/articles/{articleId}/topics

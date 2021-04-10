fc835c94c06cf04b080475c5a8268ba621b831bf
Fiona <thompsonfiona@outlook.com>
2021-04-10 10:17:32 +0200

    Begin structuring reaction

--- src/main/java/se/sdaproject/Reaction.java
+++ src/main/java/se/sdaproject/Reaction.java
@@ -1,4 +1,40 @@
 package se.sdaproject;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Reaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type;
+
+    public Reaction() {
+    }
+
+    public Reaction(Long id, String type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
---
+++ src/main/java/se/sdaproject/ReactionController.java
@@ -0,0 +1,32 @@
+package se.sdaproject;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReactionController {
+
+    //Structure should be similar to topic, but the owning side should be article/comment?
+
+    ReactionRepository reactionRepository;
+    ArticleRepository articleRepository;
+    CommentRepository commentRepository;
+
+
+    //View all reactions on an article
+
+    //View all reactions on a comment
+
+    //Add reaction to article
+
+    //Add reaction to comment
+
+    //Update reaction to article
+
+    //Update reaction to comment
+
+    //Delete reaction to article
+
+    //Delete reaction to comment
+
+
+}
